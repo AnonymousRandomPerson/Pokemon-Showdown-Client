efca839298db54464f40efe174ff5fdeacebafc5
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2017-01-07 04:58:36 -0500

    Ignored team file

--- js/teams.js
+++
@@ -1,5 +0,0 @@
-Config.team1 = "|bulbasaur|||tackle,vinewhip|||||||";
-
-Config.team2 = "|bulbasaur|grassiumz||tackle,vinewhip,frenzyplant,leechseed|||||||";
-
-Config.manualTeam = "|bulbasaur|grassiumz||tackle,vinewhip,frenzyplant,leechseed|||||||";
